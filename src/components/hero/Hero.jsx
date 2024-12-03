54f9b2c682f5ec290517e0587d18bf3047cdba02
Jessyca de Oliveira <jessycadev@gmail.com>
2024-12-03 01:17:57 -0300

    Snap Scrolling Effect Using css

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,29 @@
 import "./hero.css"
 
 const Hero = () => {
-    return (
-        <div className="">Hero</div>
-    )
+    return <div className="hero">
+        <div className="hSection left">
+            {/* TITLE */}
+            <h1 className="hTitle">Hey There,<br/><span>I'm Jessyca!</span></h1>
+            {/* AWARDS */}
+            <div className="awards">
+                <h2>Top Full Stack Developer</h2>
+                <p> More than five years of experience</p>
+            </div>
+            <div className="awardsList">
+                <img src="/award1.png" alt=""/>
+                <img src="/award2.png" alt=""/>
+                <img src="/award3.png" alt=""/>
+            </div>   
+        </div>
+        {/* SCROOL SVG */}
+        <a href="#services">
+            
+        </a>
+        <div className="hSection right">
+        </div>
+    </div>
+    
 }
 
 export default Hero
